6ee092077436974de2d49416e740cd4d1fd71133
Matthew Blake <mblake@sephiroth-2.local>
2009-02-24 00:55:02 -0500

    more stuff

--- app/controllers/stylesheets_controller.rb
+++ app/controllers/stylesheets_controller.rb
@@ -2,6 +2,13 @@ class StylesheetsController < ApplicationController
   caches_page :style
   
   def style
+    @color = Hash.new
+    @color["green"]="#5B8727"
+    @color["purple"]="#732942"
+    @color["violet"]="#A6688B"
+    @color["darkgreen"]="#253A0B"
+    @color["lightgreen"]="#9AD352"
+    @color["othercolor"]="#888888"
     @green = "#888888"
     respond_to do |format|
       format.css do
